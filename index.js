a6650830a2112b8687f6d138deee891ceeacfd30
Carlotta Chen <carlottachen92@gmail.com>
2021-12-16 10:57:35 -0800

    added rollbar

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+// include and initialize the rollbar library with your access token
+const Rollbar = require('rollbar')
+
+//create new instance of the class Rollbar
+const rollbar = new Rollbar({
+    accessToken: '081af7e73ffa49c49210b66e044149a3',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+})
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
+    //logs it when someone goes to our webpage
+    rollbar.info('html file served successfully');
 })
 
 const port = process.env.PORT || 4545
